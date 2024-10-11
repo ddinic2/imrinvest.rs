e9b2aba86ac3f7e302638980b073a7df2951f1a0
dnd_constuctive <dnd@constructive.io>
2024-10-11 15:13:06 +0200

    re-captcha and send email

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    // Mail Address and Name
+    // Mail Address and Name
+    'sender_email'   => 'ddinic2@gmail.com',
+    'sender_name'    => 'IMR Invest website',
+
+    // Mail Config
+    'mail_type'     => 'mail', // smtp or mail - mail is the php mail function
+    'smtp_server'   => 'mail.imrinvest.rs',
+    'smtp_port'     => '465',
+    'smtp_user'     => 'info@imrinvest.rs', 
+    'smtp_password' => 'G$,ubne+0Y--',
+
+    // Mail Subjects
+    'contact_form_subject' => 'New message form website',
+    'newsletter_form_subject' => 'New message form website',
+    'inquiry_form_subject' => 'New inquiry form website',
+    'inquiry_autoresponder_subject' => 'Thank you for your message.',
+
+    // Mailchimp
+    'mailchimp_support' => false, // ture is activated
+    'mailchimp_api_key' => 'xxxxxx-your-api-key-xxxxxx',
+    'mailchimp_list_id' => 'xxx-list-id-xxx',
+);
--- form.php
+++ form.php
@@ -1,13 +1,31 @@
-<?php if(isset($_POST["feedbackName"]))
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'php/Exception.php';
+require 'php/PHPMailer.php';
+require 'php/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$config = require_once 'config.php';
+
+if(isset($_POST["feedbackName"]))
 {
 	// Read the form values
 	$success = false;
 	$name = isset( $_POST['feedbackName'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['feedbackName'] ) : "";
 	$senderEmail = isset( $_POST['feedbackEmail'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['feedbackEmail'] ) : "";
-	$senderTel = isset( $_POST['feedbackTel'] ) ? preg_replace( "/[^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$]/", "", $_POST['feedbackTel'] ) : "";
+	// $senderTel = isset( $_POST['feedbackTel'] ) ? preg_replace( "/[^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$]/", "", $_POST['feedbackTel'] ) : "";
+	$senderTel = isset($_POST['feedbackTel']) ? preg_replace("/[^0-9\+\-]/", "", $_POST['feedbackTel']) : "";
+
 	$message = isset( $_POST['feedbackMessage'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['feedbackMessage'] ) : "";
 
-	echo('send')
+	
+
+	echo('send');
 	//Headers
 	$to = "ddinic2@gmail.com";
     $subject = 'Contact Us';
@@ -17,10 +35,74 @@
 	//body message
 	$message = "Name: ". $name . "<br>Email: ". $senderEmail . "<br>Phone: ". $senderTel . "<br> Message: " . $message . "";
 
-	//Email Send Function
-    $send_email = mail($to, $subject, $message, $headers);
-} else
-  {
-  	echo '<div class="failed">Failed: Email not Sent.</div>';
+
+	// re-captcha code
+	$recaptcha_secret = "6LccbF0qAAAAAJP0quCN_e103vmp5a8XnNNt1LRB";
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+
+	$recaptcha_response = $_POST['g-recaptcha-response'];
+
+	// Slanje POST zahteva ka Google reCAPTCHA serveru koristeći cURL
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, [
+    	'secret' => $recaptcha_secret,
+    	'response' => $recaptcha_response
+	]);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	// decode
+	$response_keys = json_decode($response, true);
+
+	// Provera da li je reCAPTCHA validacija uspešna
+	if (isset($response_keys['success']) && $response_keys['success']) {
+    	// reCAPTCHA je prošla, možete nastaviti sa obradom forme
+    	echo "Validacija uspešna! Možete poslati email ili obraditi podatke.";
+		sendEmail($to, $subject, $message, $headers);
+	} else {
+    	// reCAPTCHA nije prošla, prikazujemo poruku
+    	echo "Molimo potvrdite da niste robot.";
+	}
+
+	//Email Send Function TEMP OFF
+    //$send_email = mail($to, $subject, $message, $headers);
+	} else
+  	{
+  		echo '<div class="failed">Failed: Email not Sent.</div>';
+  	}
+
+  	function sendEmail($to, $subject, $message, $headers){
+		$mail = new PHPMailer(true);
+
+		try {
+			// SMTP setup
+			$mail->isSMTP();
+			$mail->Host = 'mail.imrinvest.rs';
+			$mail->SMTPAuth = true;
+			$mail->Username = 'info@imrinvest.rs';
+			$mail->Password = 'G$,ubne+0Y--';
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+			$mail->Port = 465;
+
+			$mail->setFrom('info@imrinvest.rs', 'Dejan');
+			$mail->addAddress('ddinic2@gmail.com');
+
+			$mail->isHTML(true);
+			$mail->Subject = $subject;
+			$mail->Body    = $message;
+			$mail->AltBody = $message;
+
+			$mail->send();
+			echo 'Email je uspešno poslat!!!';
+		} catch (Exception $e) {
+			echo "Email nije mogao biti poslat. Mailer Error: {$mail->ErrorInfo}";
+		}
   }
+
 ?>
+
